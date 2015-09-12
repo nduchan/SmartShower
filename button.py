9e2f09d95cee8889faf605e602616ca4323da94a
Spencer Maroukis <smarouki@umich.edu>
2015-09-12 19:39:10 -0400

    Fixed callback functions

--- button.py
+++ button.py
@@ -5,9 +5,9 @@ import os
 led=22
 bttn=18
 
-GPIO.setup(bttn, GPIO.IN, initial=False) # initial button is off
-GPIO.setup(led, GPIO.OUT, initial=False)  # initial led is off
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(bttn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initial button is off
+GPIO.setup(led, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)  # initial led is off
 
 def button_state():
 	while True:
@@ -15,12 +15,12 @@ def button_state():
 		time.sleep(0.5)
 		return shower_state
 		GPIO.output(led,shower_state)
+		GPIO.add_event_detect(bttn, GPIO.RISING, callback=my_callback_closed, bouncetime=500)
+		GPIO.add_event_detect(bttn, GPIO.FALLING, callback=my_callback_open, bouncetime=500)
 
 
 def my_callback_closed():	
 	print('Somebody entered the shower')
-	GPIO.add_event_detect(bttn, GPIO.RISING, callback=my_callback_closed, bouncetime=500)
 
 def my_callback_open():
 	print('Somebody exited the shower')
-	GPIO.add_event_detect(bttn, GPIO.FALLING, callback=my_callback_open, bouncetime=500)
