1c96ae7f50976beb896c0725146b99dde3341868
devenkishore <dkishore@umich.edu>
2015-09-12 20:13:17 -0400

    working on states

--- pi.py
+++ pi.py
@@ -1,7 +1,108 @@
 ﻿
 import datetime
-import RPi.GPIO as GPIO
-import time
+#import RPi.GPIO as GPIO
+import sys
 
-if __name__ == "__main__": main()
 
+def current_time(): # returns current hour, min, and second in military time
+    RN = datetime.datetime.now()
+    now = datetime.time(RN.hour, RN.minute, RN.second)
+    return now
+
+
+def set_timeline(): # makes the timeline 
+
+    ############ User 1
+    user1 = 'Deven'
+    user1_start_time = datetime.time(20,00,0)
+    user1_end_time = datetime.time(20,31,0)
+
+    ############ User 2
+    user2 = 'Albert'
+    user2_start_time = datetime.time(20,31,0)
+    user2_end_time = datetime.time(20, 44,0)
+
+    ############ User 3
+    user3 = 'Spencer'
+    user3_start_time = datetime.time(20,46,0)
+    user3_end_time = datetime.time(20,51,0)
+
+    ############ User 4
+    user4 = 'Noah'
+    user4_start_time = datetime.time(20,51,0)
+    user4_end_time = datetime.time(20,59,0)
+
+    timeline = [[user1, user1_start_time, user1_end_time],
+            [user2, user2_start_time, user2_end_time],
+            [user3, user3_start_time, user3_end_time],
+            [user4, user4_start_time, user4_end_time]]
+    return timeline
+
+def print_timeline(timeline):
+    for i in timeline:
+        print "name:", i[0]
+        print "start time:" , i[1]
+        print "end time: " , i[2]
+        print 
+
+def determine_current_turn(timeline):
+    for i in timeline:
+        if (i[1] < current_time() < i[2]):
+            return i 
+    return False
+
+
+
+
+def state_UO(): # U & open, no problems, normal operations
+    print "UO"
+    return
+
+def state_UC(): #U & Closed, no problems, normal operations
+    print "UC"
+    return
+
+def state_SO(scheduled_user): # scheduled and open, 
+    print "SO"
+    print scheduled_user, "is scheduled to use shower, but has not begun yet"
+    return    
+
+def state_SC(scheduled_user, scheduled_user_end_time):
+    print "SC"
+    print scheduled_user_end_time
+    warning_time = scheduled_user_end_time - datetime.timedelta(minutes=1)
+    while (current_time() < warning_time): # nothing wrong, user in shower
+        while shower_state == True:
+            print scheduled_user, "is scheduled and currently using...waiting..."
+            if (current_time() == warning_time) & (shower_state == True):
+                print "one min left"
+                if ((current_time() > warning_time) & (shower_state == True)):
+                    print scheduled_user, "is going over time"
+
+        if shower_state == False:
+            return
+
+def main():
+    shower_state = True
+    timeline = set_timeline()
+    print_timeline(timeline)
+    if determine_current_turn(timeline) == False: # unscheduled
+        if shower_state == False: # open U.O.
+            state_UO()
+        elif shower_state == True: # closed U.C.
+            state_UC()
+    else:  # scheduled
+        scheduled_user = determine_current_turn(timeline)[0]
+        scheduled_user_end_time = determine_current_turn(timeline)[2]
+        print "Current shower time belongs to" , scheduled_user, "because the time is", current_time()
+        if shower_state == False: # open S.O.
+            state_SO(scheduled_user)
+
+        elif shower_state == True: # closed S.C.
+            state_SC(scheduled_user, scheduled_user_end_time)
+            
+
+
+
+if __name__ == "__main__":
+    main()
