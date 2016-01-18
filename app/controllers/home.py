cf74663ae6fbb704594121cc9d6e6fd3c8f2a373
Albert Yerman <ahyerman@umich.edu>
2016-01-17 23:08:31 -0500

    changes to signup link

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,3 +1,4 @@
+import flask
 from flask import *
 from flask.ext.mysqldb import MySQL
 from flask import render_template
@@ -8,7 +9,10 @@ home = Blueprint('home', __name__, template_folder='views')
 
 @home.route('/home')
 def display_home():
-    if flask.session['signed_in']:
-        return render_template('home.html', calendar_id=json.dumps(calendar_id))
-    else:
-        return render_template('index.html')
+    try:
+    	if flask.session['signed_in']:
+        	return render_template('home.html', calendar_id=json.dumps(calendar_id))
+    	else:
+        	return render_template('index.html')
+    except:
+    	return render_template('index.html')
