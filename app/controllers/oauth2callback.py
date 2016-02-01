104e9f4c03b4e83f0c4ca3e90d46c032a6ea1104
Albert Yerman <ahyerman@umich.edu>
2016-01-31 21:52:27 -0500

    fixed problem with oauth. noe correctly sends request to google api

--- app/controllers/oauth2callback.py
+++ app/controllers/oauth2callback.py
@@ -19,18 +19,20 @@ oauth2callback = Blueprint('oauth2callback', __name__, template_folder='views')
 
 
 @oauth2callback.route('/oauth2callback')
-def oauth_approval():
+def oauth():
     flow = client.flow_from_clientsecrets(
                 CLIENT_SECRET_FILE,
                 SCOPE,
-                redirect_uri=url_for('oauth2callback', _external=True)
+                redirect_uri=url_for('oauth2callback.oauth', _external=True)
                 )
     if 'code' not in request.args:
         auth_uri = flow.step1_get_authorize_url()
+        print "=============AUTH_URI = %s", auth_uri
         return redirect(auth_uri)
     else:
         auth_code = request.args.get('code')
         credentials = flow.step2_exchange(auth_code)
         session['credentials'] = credentials.to_json()
         caller = session['oauth_caller']
+        print "%s", caller
         return redirect(url_for(caller))
