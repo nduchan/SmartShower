677b69a8103da8a8f64f3dc894251b2b24a74478
Spencer Maroukis <smarouki@umich.edu>
2016-03-12 23:46:46 -0500

    moved address database variable input to signup.py route

--- app/controllers/signup.py
+++ app/controllers/signup.py
@@ -18,8 +18,9 @@ def signUp():
     _lname = request.form['inputLName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
+    _address = request.form['inputAddress']
 
-    if _fname and _lname and _email and _password:
+    if _fname and _lname and _email and _password and _address:
 
         conn = mysql.connection
         cursor = conn.cursor()
@@ -31,8 +32,8 @@ def signUp():
         if exists:
             return render_template('signup.html', exists=True);
         else:
-            sql = "INSERT INTO SmartShowerDB.users (fname, lname, email, password) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (_fname, _lname, _email, _password))
+            sql = "INSERT INTO SmartShowerDB.users (fname, lname, email, password, address) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, (_fname, _lname, _email, _password, _address))
             conn.commit()
             cursor.execute("SELECT LAST_INSERT_ID()")
             result = cursor.fetchall()
@@ -41,6 +42,7 @@ def signUp():
             session['fname'] = _fname
             session['lname'] = _lname
             session['signed_in'] = True
+            session['address'] = _address
             #return render_template('completeProfile.html', last_id=last_id)
             return redirect('/update_profile')
 
